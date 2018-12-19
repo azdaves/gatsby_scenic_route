23571f6d3faa87a02351a262c332107203e9637e
Dave Schilling <ds@Daves-MacBook-Pro.local>
2018-12-19 09:58:14 -0700

    form fix2

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Image from '../components/image'
 
 const BlogPage = ({ data }) => (
   <Layout>
+        
   <div>
     <h1>Latest Posts</h1>
+    <div style={{ maxWidth: '100px', marginBottom: '0' }}>
+    <Image />
+    </div>
+    
     {data.allMarkdownRemark.edges.map(post => (
       <div key={post.node.id}>
         <h3>{post.node.frontmatter.title}</h3>
